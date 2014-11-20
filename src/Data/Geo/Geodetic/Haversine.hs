f3fae8a3813b320a446c3fb91ae75dc9d09e5421
Tony Morris <tmorris@tmorris.net>
2014-11-20 12:43:20 +1000

    Haversine

--- src/Data/Geo/Geodetic/Haversine.hs
+++ src/Data/Geo/Geodetic/Haversine.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 
 -- | Haversine geodetic distance algorithm.
 module Data.Geo.Geodetic.Haversine(
@@ -7,61 +9,37 @@ module Data.Geo.Geodetic.Haversine(
 , haversine'
 ) where
 
-import Prelude(Double, Num(..), Fractional(..), (.), pi, atan, sin, atan2, cos, sqrt)
+import Control.Applicative(Const)
+import Prelude(Double, Num(..), Fractional(..), (.), pi, sin, atan2, cos, sqrt)
 import Control.Lens((#), (^.))
 import System.Args.Optional(Optional1(..))
 import Data.Geo.Coordinate
 import Data.Geo.Geodetic.Sphere
 
 -- $setup
--- >>> import Prelude(Functor(..), Monad(..), String)
+-- >>> import Prelude(Functor(..), Monad(..), String, Double)
 -- >>> import Data.Maybe(Maybe)
 -- >>> import Text.Printf(printf)
 
 -- | Haversine algorithm.
 --
--- >>> fmap (printf "%0.4f") (do fr <- 27.812 ..#.. 154.295; to <- (-66.093) ..#.. 12.84; return (haversine earthMean fr to)) :: Maybe String
+-- >>> fmap (printf "%0.4f") (do fr <- 27.812 <°> 154.295; to <- (-66.093) <°> 12.84; return (haversine earthMean fr to)) :: Maybe String
 -- Just "15000950.5589"
 --
--- >>> fmap (printf "%0.4f") (do fr <- (-16.7889) ..#.. 41.935; to <- 6.933 ..#.. (-162.55); return (haversine earthMean fr to)) :: Maybe String
+-- >>> fmap (printf "%0.4f") (do fr <- (-16.7889) <°> 41.935; to <- 6.933 <°> (-162.55); return (haversine earthMean fr to)) :: Maybe String
 -- Just "17128743.0669"
 --
--- >>> fmap (printf "%0.4f") (do fr <- 27.812 ..#.. 154.295; to <- (-66.093) ..#.. 12.84; return (haversine (6350000 ^. nSphere) fr to)) :: Maybe String
+-- >>> fmap (printf "%0.4f") (do fr <- 27.812 <°> 154.295; to <- (-66.093) <°> 12.84; return (haversine ((6350000 :: Double) ^. _Sphere) fr to)) :: Maybe String
 -- Just "14959840.4461"
 --
--- >>> fmap (printf "%0.4f") (do fr <- (-16.7889) ..#.. 41.935; to <- 6.933 ..#.. (-162.55); return (haversine (6350000 ^. nSphere) fr to)) :: Maybe String
+-- >>> fmap (printf "%0.4f") (do fr <- (-16.7889) <°> 41.935; to <- 6.933 <°> (-162.55); return (haversine ((6350000 :: Double) ^. _Sphere) fr to)) :: Maybe String
 -- Just "17081801.7377"
-
-{-}
 haversine ::
-  AsCoordinate (->) f start =>
+  (AsCoordinate (->) (Const Coordinate) start, AsCoordinate (->) (Const Coordinate) end) =>
   Sphere -- ^ reference sphere
   -> start -- ^ start coordinate
-  -> Coordinate -- ^ end coordinate
+  -> end -- ^ end coordinate
   -> Double
-  -}
--- todo  
-{-}
-  haversine :: forall s s1 a.
-                   (AsCoordinate (->) (Control.Applicative.Const Coordinate) s1,
-                    AsCoordinate (->) (Control.Applicative.Const Coordinate) s,
-                    AsLatitude
-                      tagged-0.7.3:Data.Tagged.Tagged
-                      transformers-0.3.0.0:Data.Functor.Identity.Identity
-                      a,
-                    AsLongitude
-                      tagged-0.7.3:Data.Tagged.Tagged
-                      transformers-0.3.0.0:Data.Functor.Identity.Identity
-                      a,
-                    AsSphere
-                      tagged-0.7.3:Data.Tagged.Tagged
-                      transformers-0.3.0.0:Data.Functor.Identity.Identity
-                      a,
-                    GHC.Float.RealFloat a) =>
-                   Sphere -> s1 -> s -> a
-
-
--}
 haversine s start' end' =
   let start = start' ^. _Coordinate
       end = end' ^. _Coordinate
@@ -78,29 +56,25 @@ haversine s start' end' =
 
 -- | Haversine algorithm with a default sphere of the earth mean.
 --
--- >>> fmap (printf "%0.4f") (do fr <- 27.812 ..#.. 154.295; to <- (-66.093) ..#.. 12.84; return (haversineD fr to)) :: Maybe String
+-- >>> fmap (printf "%0.4f") (do fr <- 27.812 <°> 154.295; to <- (-66.093) <°> 12.84; return (haversineD fr to)) :: Maybe String
 -- Just "15000950.5589"
 --
--- >>> fmap (printf "%0.4f") (do fr <- (-16.7889) ..#.. 41.935; to <- 6.933 ..#.. (-162.55); return (haversineD fr to)) :: Maybe String
+-- >>> fmap (printf "%0.4f") (do fr <- (-16.7889) <°> 41.935; to <- 6.933 <°> (-162.55); return (haversineD fr to)) :: Maybe String
 -- Just "17128743.0669"
-{-
 haversineD ::
-  (HasCoordinate c1, HasCoordinate c2) =>
-  c1 -- ^ start coordinate
-  -> c2 -- ^ end coordinate
+  (AsCoordinate (->) (Const Coordinate) start, AsCoordinate (->) (Const Coordinate) end) =>
+  start -- ^ start coordinate
+  -> end -- ^ end coordinate
   -> Double
-  -}
 haversineD =
-  undef -- haversine earthMean
-
-undef = undef  
+  haversine earthMean
 
 -- | Haversine algorithm with an optionally applied default sphere of the earth mean.
 --
--- >>> fmap (printf "%0.4f") (do fr <- 27.812 ..#.. 154.295; to <- (-66.093) ..#.. 12.84; return (haversine' fr to :: Double)) :: Maybe String
+-- >>> fmap (printf "%0.4f") (do fr <- 27.812 <°> 154.295; to <- (-66.093) <°> 12.84; return (haversine' fr to :: Double)) :: Maybe String
 -- Just "15000950.5589"
 --
--- >>> fmap (printf "%0.4f") (do fr <- (-16.7889) ..#.. 41.935; to <- 6.933 ..#.. (-162.55); return (haversine' fr to :: Double)) :: Maybe String
+-- >>> fmap (printf "%0.4f") (do fr <- (-16.7889) <°> 41.935; to <- 6.933 <°> (-162.55); return (haversine' fr to :: Double)) :: Maybe String
 -- Just "17128743.0669"
 haversine' ::
   (Optional1
